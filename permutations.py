4fc04798c0b111a505caf12aaade490d23e02c47
Zeel Prajapati <43785480+zack0705@users.noreply.github.com>
2021-09-23 10:56:34 -0700

    Update permutations.py

--- permutations.py
+++ permutations.py
@@ -1,33 +1,10 @@
-def swap(a,i):
-    if(i==0):
-        return a
-    x = a[i] + a[1:i] + a[0] + a[i+1:]
-    return x
-    
-def permu(a1):
-    ans = []
-    if(len(a1)==2):
-        x = []
-        b = a1[1] + a1[0]
-        x.append(b)
-        return x
-    else:
-        for i in range(len(a1)):
-            a = swap(a1,i)
-            x = a[:1-len(a1)]
-            y = a[1-len(a1):]
-            b = x + y
-            ans.append(b)
-            y = permu(y)
-            for j in y:
-                b = x + j
-                if b not in ans:
-                    ans.append(b)
-            
-        return ans
-        
-a = permu("abcdef")
-print(a)
-print(len(a))
-        
-
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        if(len(nums)==1 or len(nums)==0):
+            return([nums])
+        if(len(nums)==2):
+            return([nums]+[nums[::-1]])
+        for i in range(len(nums)):
+            ans += [[nums[i]]+j for j in self.permute(nums[:i]+nums[i+1:])]
+        return(ans)
